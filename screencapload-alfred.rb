bd4d31831e482ae1f3fa61567323f83bd7fcb2c7
Clement Joubert <clement.joubert@gmail.com>
2015-03-16 11:00:57 +0100

    Added Alfred workflow based on screencapload
    In the workflow, an API key is hard coded.
    The script looks for a local key ~/.imgur then falls back.

---
+++ screencapload-alfred.rb
@@ -0,0 +1,46 @@
+# Code adapted from the screencapload project
+# https://github.com/czj/screencapload
+#
+# imgur API key used here will probably be used more than 100 times / hour
+# so we'll need to request another one, or unlimited access.
+
+config_file = "#{ENV['HOME']}/.imgur"
+tempfile = "/tmp/screenshot-#{Time.now.to_f}.png"
+trimmed_tempfile = "#{tempfile}-trimmed.png"
+
+api_key = if File.exists?(config_file)
+  File.read(config_file).strip
+else
+  '5d900ac2731b5fb'
+end
+
+# Take a screenshot on OSX
+`/usr/sbin/screencapture -x -i '#{tempfile}'`
+
+# Check that the screenshot was actually captured, exit if not
+unless File.exists?(tempfile)
+  exit! 1
+end
+
+# Trim the image
+if File.exists?(gm = "/usr/local/bin/gm")
+  `#{gm} convert "#{tempfile}" -trim "#{trimmed_tempfile}" && mv "#{trimmed_tempfile}" "#{tempfile}"`
+end
+
+# Recompress PNG for faster upload/download
+if File.exists?(optipng = "/usr/local/bin/optipng")
+  `#{optipng} "#{tempfile}"`
+end
+
+# Upload to imgur
+response = `curl --header 'Authorization: Client-ID #{api_key}' --silent -X POST -F 'image=@#{tempfile}' 'https://api.imgur.com/3/upload.xml'`
+
+# Remove tempfile
+`rm #{tempfile}`
+
+# Parse response and get original image url from XML
+require "rexml/document"
+url = REXML::Document.new(response).root.elements['link'].text
+
+# Alfred will copy the URL to the clipboard and paste it to frontmost app
+print url
